aeaf25bf9ffa02f12d587924949630a52eeab162
BinaryKenny <khairullinvadim2006@gmail.com>
2025-11-24 21:08:23 +0300

    I deleted mistakes and made first 'make' as a comment

--- main.cpp
+++ main.cpp
@@ -4,8 +4,19 @@ struct Planar
 {
   virtual int x() const = 0;
   virtual int y() const = 0;
-  virtual int abc_sqr() const = 0;
+  virtual int abs_sqr() const = 0;
   virtual ~Planar() = default;
+};
+
+struct Point: Planar
+{
+  virtual int x() const;
+  virtual int y() const;
+  virtual int abs_sqr() const;
+  Point(int xx, int yy);
+  virtual ~Point() = default;
+private:
+  int data[2];
 };
 
 Planar * make(std::istream & is);
@@ -18,7 +29,7 @@ int main()
 {
   Planar * pls[10] = {};
   size_t k = 0;
-  for (size_t i = 0, i < 10; i++)
+  for (size_t i = 0; i < 10; i++)
   {
     try
     {
@@ -35,19 +46,17 @@ int main()
   free_planars(pls, k);
   //pls[k++] = make(std::cin);
   //Planar * left = most_left(pls, k);
-  //draw(left);
-  //free_planars(pls,k);
 }
 
-Planar * make (size_t pl)
-{
-  switch (pl)
-  {
-    default:
-      throw std::logic_error("bad id");
-  }
-  return nullptr;
-}
+//Planar * make (size_t pl)
+//{
+  //switch (pl)
+  //{
+    //default:
+      //throw std::logic_error("bad id");
+  //}
+  //return nullptr;
+//}
 
 void free_planars(Planar ** pls, size_t k)
 {
@@ -58,12 +67,45 @@ void free_planars(Planar ** pls, size_t k)
 }
 void draw(Planar * pls)
 {
-  std::cout << pl->x();
+  std::cout << pls->x();
   std::cout << " ";
-  std::cout << pl->y() << "\n";
+  std::cout << pls->y() << "\n";
 }
 
 Planar * most_left(Planar ** pls, size_t k)
 {
   return nullptr;
+}
+
+Point::Point(int xx, int yy):
+  Planar(),
+  data{xx, yy}
+{}
+int Point::x() const
+{
+  return data[0];
+}
+
+int Point::y() const
+{   
+  return data[1];
+}  
+
+int Point::abs_sqr() const
+{
+  return x() * x() + y() * y();
+}
+
+Planar * make(size_t id)
+{
+  Planar * r = nullptr;
+  switch(id)
+  {
+    case 0:
+      r = new Point(0,0);
+      break;
+    default:
+      throw std::logic_error("bad id");
+  }
+  return r;
 }
